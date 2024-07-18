00913c223e0fad6a49d60c4202dc7c9c92731956
akkas_dev <codersadhin@gmail.com>
2024-07-19 03:00:02 +0800

    complete basic layout with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import {Routes, Route} from 'react-router-dom'
+
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_root/RootLayout'
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout/>}>
+                <Route path='/sign_in' element={<SigninForm />} />
+                <Route path='/sign_in' element={<SignupForm />} />
+            </Route>
+            
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
